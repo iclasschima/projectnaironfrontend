9c660ce468eab298b0f4a69c7b018f16dcb80a25
omoelyon <ganiyuelegbede@yahoo.com>
2020-03-30 15:29:20 +0100

    project structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import SignUp from "./Signup";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <SignUp />
+      </div>
+    </div>
+  );
+}
+
+export default App;
